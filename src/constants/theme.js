c21654d41d1aaca4c0892611a0e6eabd871037fd
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-07-08 02:15:42 -0400

    created initial themeing of application. Subject to massive change

---
+++ src/constants/theme.js
@@ -0,0 +1,21 @@
+const theme = {
+	palette: {
+		primary: {
+			main: '#DD2A7B', // Deep pink
+			light: '#C13584', // Magenta
+			dark: '#833AB4', // Dark Violet
+		},
+		secondary: {
+			main: '#FD1D1D', // Red
+			light: '#F56040', // Light Orange
+			dark: '#ED4C67', // Pink
+		},
+		text: {
+			primary: '#262626', // Black
+			secondary: '#FFFFFF', // White
+		},
+		// Add more colors as needed
+	},
+};
+
+export default theme;
