63ed9373a947e99d04ddeae596d057165ea9c3d1
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-07-08 01:36:08 -0400

    added a writing component

---
+++ src/components/WritingComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WritingComponent = () => {
+  return (
+    <div>
+        We will build out the writing component here.
+        It will be fleshed out and have the power of a google docs type of editor.
+    </div>
+    )
+}
+
+export default WritingComponent
