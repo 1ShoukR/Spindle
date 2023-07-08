7c1248af9401edfff25f8049ab0cae17eab128c6
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-07-08 00:59:04 -0400

    added classes for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../css/navbar.css'
 
 const Navbar = () => {
   return (
-    <nav >
-      <div>
+    <nav className='nav-container'>
+      <div className='left-section'>
         <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
         </ul>
       </div>
-      <div>
+      <div className='right-section'>
         <ul>
           <li>
             <Link to="/create-account">Create Account</Link>
